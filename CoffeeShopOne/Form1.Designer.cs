15e8c65949dc5cf299df4cf8d136d2c329803908
Your Name <you@example.com>
2022-02-04 01:01:19 +0600

    Add project files.

---
+++ CoffeeShopOne/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace CoffeeShopOne
+{
+    partial class CoffeeShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customerNameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.coffeeTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // customerNameTextBox
+            // 
+            this.customerNameTextBox.Location = new System.Drawing.Point(236, 92);
+            this.customerNameTextBox.Name = "customerNameTextBox";
+            this.customerNameTextBox.Size = new System.Drawing.Size(189, 20);
+            this.customerNameTextBox.TabIndex = 0;
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(236, 138);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(189, 20);
+            this.phoneNumberTextBox.TabIndex = 0;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(236, 192);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(189, 20);
+            this.addressTextBox.TabIndex = 0;
+            // 
+            // coffeeTypeComboBox
+            // 
+            this.coffeeTypeComboBox.FormattingEnabled = true;
+            this.coffeeTypeComboBox.Items.AddRange(new object[] {
+            "Black Coffee",
+            "Cold Coffee",
+            "Hot Coffee",
+            "Regular Coffee"});
+            this.coffeeTypeComboBox.Location = new System.Drawing.Point(236, 241);
+            this.coffeeTypeComboBox.Name = "coffeeTypeComboBox";
+            this.coffeeTypeComboBox.Size = new System.Drawing.Size(189, 21);
+            this.coffeeTypeComboBox.TabIndex = 1;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(236, 292);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(189, 20);
+            this.quantityTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Customer Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Phone Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(120, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Coffee Type:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(132, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Quantity:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(270, 340);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(96, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(485, 83);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(286, 248);
+            this.richTextBox.TabIndex = 5;
+            this.richTextBox.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(285, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(205, 37);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Coffee Shop";
+            // 
+            // CoffeeShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.coffeeTypeComboBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.customerNameTextBox);
+            this.Name = "CoffeeShop";
+            this.Text = "Coffee Shop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox customerNameTextBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.ComboBox coffeeTypeComboBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ CoffeeShopOne/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeeShopOne
+{
+    public partial class CoffeeShop : Form
+    {
+        int black = 120;
+        int cold = 100;
+        int hot = 90;
+        int regular = 80;
+        int total;
+
+        public int Total { get => total; set => total = value; }
+
+        public CoffeeShop()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string CustomerName = customerNameTextBox.Text,
+                PhoneNumber = phoneNumberTextBox.Text,
+                Address = addressTextBox.Text,
+                CoffeeType = coffeeTypeComboBox.Text;
+            int qua = Convert.ToInt32(quantityTextBox.Text);
+
+         
+
+         
+            if (CoffeeType == "Black Coffee")
+            {
+                total = qua * black;
+            }else if (CoffeeType == "Cold Coffee")
+            {
+                total = qua * cold;
+            }else if(CoffeeType == "Hot Coffee")
+            {
+                total = qua * hot;
+            }else if(CoffeeType == "Regular Coffee")
+            {
+                total = qua * regular;
+            }
+
+            
+
+            if (CustomerName == "")
+            {
+                MessageBox.Show("Please Enter Customer Name");
+            }
+            else if (PhoneNumber == "")
+            {
+                MessageBox.Show("Please Enter Phone Number");
+            }
+            else if (Address == "")
+            {
+                MessageBox.Show("Please Enter Address");
+            }
+            else if (CoffeeType == "")
+            {
+                MessageBox.Show("Please Select Coffee Type");
+            }
+            else if (qua < 0)
+            {
+                MessageBox.Show("Please Enter Quantitly");
+            }
+            else
+            {
+                richTextBox.Text = ("Customer Name: " + CustomerName + "\n" +  " \n " + "Phone Number: " + PhoneNumber + " \n " + " \n " + "Address: " + Address + " \n " + " \n " + " \n " + "Coffee Type: " + CoffeeType + "\n" +  " \n " + "Quantity: " + qua +" \n " + " \n "+"Total Price: " + total);
+            }
+
+        }
+    }
+}
